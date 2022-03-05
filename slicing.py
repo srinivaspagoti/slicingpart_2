8678fc81cd4a65b0dae22b853d5df00463f8b6f2
p srinivas <100501528+srinivaspagoti@users.noreply.github.com>
2022-03-05 09:18:35 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,53 @@
+# print(len("srinivas"))
+# a=("python")
+# print(a,type(a))
+# print(a[0])
+
+# print("python"[0])
+# print("python"[1])
+# print("python"[2])
+# print("python"[3])
+# print("python"[4])
+# print("python"[5])
+# print("srinivasnaidu"[9])
+# print('sriniasnaidu'[15])
+
+# x="delllaptop"
+# print(x[1]+x[4]+x[3]+x[2]+x[5]+x[8]+x[6]+x[7]+x[9])
+
+# x="python"
+# print(x[-6:-1])
+# print(x[-2:-6])
+# print(x[1:-5])
+# print(x[-6:0])
+# print(x[-5:0])
+# print(x[0:5])
+
+a="python"
+# print(a[ : :5])
+# print(a[ : :4])
+# print(a[ : :3])
+# print(a[ : :2])
+# print(a[ : :1])
+# print(a[ : :0])
+# print(a[ : :-1])
+# print(a[ : :-2])
+# print(a[ : :-3])
+# print(a[ : :-4])
+# print(a[ : :-5])
+# print(a[ : :-6])
+
+# print(a[0: : ])
+# print(a[1: : ])
+# print(a[2: : ])
+# print(a[3: : ])
+# print(a[4: : ])
+# print(a[5: : ])
+# print(a[6: : ])
+
+print(a[1:6: ])
+print(a[2:5: ])
+print(a[3:4: ])
+print(a[4:3: ])
+print(a[5:2: ])
+print(a[6:1: ])
